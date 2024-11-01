205f802152f7760ceae9002393740579b6af5c66
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-11-01 08:04:00 -0700

    move pool into the db directory

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 require("dotenv").config();
+
 const path = require("node:path");
-const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const usersRouter = require("./routes/users");
+const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const { loadEnvFile } = require("node:process");
+const { clear } = require("node:console");
+// const { loadEnvFile } = require("node:process");
 
-const { PORT, USER, HOST, DATABASE, PASSWORD, SQL_PORT, SESSION_KEY } =
-  process.env;
-
-const pool = new Pool({
-  user: USER,
-  host: HOST,
-  database: DATABASE,
-  password: PASSWORD,
-  port: SQL_PORT,
-});
-
+const { PORT, SESSION_KEY } = process.env;
+console.log(PORT, SESSION_KEY);
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -28,8 +21,7 @@ app.use(
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
-
 app.use("/users", usersRouter);
+app.get("/", (req, res) => res.render("index"));
 
 app.listen(PORT, () => console.log("app listening on port 3000!"));
