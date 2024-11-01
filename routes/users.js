048ecbac61627b400acabc9ef2e975c34d236336
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-11-01 08:08:07 -0700

    add post to submit to db

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
+const pool = require("../db/pool");
 
+router.get("/signup", (req, res) => {
+  res.render("sign-up-form");
+});
 
+router.post("/signup", async (req, res, next) => {
+  // creating users unsafely to test authentication
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
 
-router.get("/signup", (req, res) {
-res.render("sign-up-form"));
-
-
-modeule.exports = router;
+module.exports = router;
