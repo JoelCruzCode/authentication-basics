2dc3d0a71980a26c67fa551ac3006b6aa3f069f1
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2024-11-03 17:04:38 -0800

    remove hard coded info

--- db/pool.js
+++ db/pool.js
@@ -1,23 +1,14 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// Log the entire process.env object for debugging
-console.log("Environment Variables:", process.env);
-
-// const { USER, HOST, DATABASE, PASSWORD, SQL_PORT } = process.env;
+const { USER, HOST, DATABASE, PASSWORD, SQL_PORT } = process.env;
 
 const pool = new Pool({
-  user: "hydro",
-  host: "localhost",
-  database: "authentication_basics",
-  password: "pump",
-  port: 5555,
-
-  //  user: USER,
-  //  host: HOST,
-  // database: DATABASE,
-  //  password: PASSWORD,
-  //  port: SQL_PORT,
+  user: USER,
+  host: HOST,
+  database: DATABASE,
+  password: PASSWORD,
+  port: SQL_PORT,
 });
 
 module.exports = pool;
